69e154db05b206bb7a65987ae31b9f223f2fb03a
Joshua Smith <kernelsmith@metasploit.com>
2014-10-07 18:30:20 -0500

    adds some path/gem helps to lib/skyrack.rb

--- lib/skyrack.rb
+++ lib/skyrack.rb
@@ -1,3 +1,20 @@
+
+#require 'pathname'
+#require Pathname.new(__FILE__).realpath.expand_path.parent.join('lib')
+binbase = __FILE__
+while File.symlink?(binbase)
+  binbase = File.expand_path(File.readlink(binbase), File.dirname(binbase))
+end
+$:.unshift(File.expand_path(File.join(File.dirname(binbase), 'lib', 'skyrack')))
+
+require 'functions'
+require 'gadget'
+require 'gadget_db'
+require 'instr'
+require 'payload'
+require 'roper'
+require 'version'
+
 module Skyrack
-  # Your code goes here...
+  # your code here
 end
